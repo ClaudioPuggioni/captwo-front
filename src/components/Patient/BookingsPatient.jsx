d87b3daa7727e5ef5a921de8a50a4b02823b5007
Claudio Puggioni <claudieddu@hotmail.com>
2022-10-11 10:55:33 +0700

    fixing redirects, then scheduling

--- src/components/Patient/BookingsPatient.jsx
+++ src/components/Patient/BookingsPatient.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 import TextField from "@mui/material/TextField";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
@@ -30,83 +31,83 @@ export default function BookingsDoctor() {
   const { userType, userInfo } = useSelector((state) => state.wall);
   const { toBookDoctor, chosenDoctor, loading } = useSelector((state) => state.cabinet);
   const dispatch = useDispatch();
-  const [value, setValue] = useState(dayjs());
-  const [timetable, setTimetable] = useState([]);
-  const [timeNow, setTimeNow] = useState(new Date().toString());
+  const [value, setValue] = useState(dayjs.extend(utc).utc());
+  const [timetable, setTimetable] = useState(null);
+  const [timeNow, setTimeNow] = useState(new Date().toUTCString());
   const [bookedSlot, setBookedSlot] = useState([null, null]);
   const [consultReason, setConsultReason] = useState("");
 
-  const available_times_Parser = function (inputObj) {
-    const thisWeek = {};
-    const nextWeek = {};
-    const currentDay = new Date().toUTCString();
-    const currWeekdaySLUG3 = currentDay.slice(0, 3).toLocaleLowerCase();
-    let currDayIdx = weekdayOrder.findIndex((ele) => ele.slice(0, 3) === currWeekdaySLUG3);
-    let isNextDay = true;
+  // const available_times_Parser = function (inputObj) {
+  //   const thisWeek = {};
+  //   const nextWeek = {};
+  //   const currentDay = new Date().toUTCString();
+  //   const currWeekdaySLUG3 = currentDay.slice(0, 3).toLowerCase();
+  //   let currDayIdx = weekdayOrder.findIndex((ele) => ele.slice(0, 3) === currWeekdaySLUG3);
+  //   let isNextDay = true;
 
-    const orderedWeek = [];
+  //   const orderedWeek = [];
 
-    for (const weekdayIdx in weekdayOrder) {
-      orderedWeek[weekdayIdx] = { [weekdayOrder[weekdayIdx]]: inputObj[weekdayOrder[weekdayIdx]] };
-    }
+  //   for (const weekdayIdx in weekdayOrder) {
+  //     orderedWeek[weekdayIdx] = { [weekdayOrder[weekdayIdx]]: inputObj[weekdayOrder[weekdayIdx]] };
+  //   }
 
-    // console.log(orderedWeek);
+  //   // console.log(orderedWeek);
 
-    for (const weekdayObj of orderedWeek) {
-      // console.log(weekdayObj, currDayIdx);
-      const weekday = Object.keys(weekdayObj)[0];
-      const daySlots = Object.values(weekdayObj)[0];
-      // console.log("WEEKDAY:", weekday);
-      // console.log("DAYSLOTS:", daySlots);
-      if (Object.values(daySlots).length !== 0) {
-        const weekIdx = weekdayOrder.findIndex((ele) => ele === weekday);
-        if (weekIdx < currDayIdx) {
-          // console.log([weekIdx, currDayIdx]);
-          // nextWeek.push(weekdayObj);
-          nextWeek[weekday] = daySlots;
-        } else {
-          // split day into two if timeslot is past current time
-          if (isNextDay) {
-            isNextDay = false;
+  //   for (const weekdayObj of orderedWeek) {
+  //     // console.log(weekdayObj, currDayIdx);
+  //     const weekday = Object.keys(weekdayObj)[0];
+  //     const daySlots = Object.values(weekdayObj)[0];
+  //     // console.log("WEEKDAY:", weekday);
+  //     // console.log("DAYSLOTS:", daySlots);
+  //     if (Object.values(daySlots).length !== 0) {
+  //       const weekIdx = weekdayOrder.findIndex((ele) => ele === weekday);
+  //       if (weekIdx < currDayIdx) {
+  //         // console.log([weekIdx, currDayIdx]);
+  //         // nextWeek.push(weekdayObj);
+  //         nextWeek[weekday] = daySlots;
+  //       } else {
+  //         // split day into two if timeslot is past current time
+  //         if (isNextDay) {
+  //           isNextDay = false;
 
-            const thisWeekToday = {};
-            const nextWeekToday = {};
+  //           const thisWeekToday = {};
+  //           const nextWeekToday = {};
 
-            let regex = /\d\d:\d\d/;
-            const currTimeArr = currentDay.match(regex)[0].split(":");
-            const currTime = new Date();
+  //           let regex = /\d\d:\d\d/;
+  //           const currTimeArr = currentDay.match(regex)[0].split(":");
+  //           const currTime = new Date();
 
-            const timeslots = Object.entries(daySlots);
-            for (let index = 0; index < timeslots.length; index++) {
-              // console.log("timeslots[index]:", timeslots[index]);
+  //           const timeslots = Object.entries(daySlots);
+  //           for (let index = 0; index < timeslots.length; index++) {
+  //             // console.log("timeslots[index]:", timeslots[index]);
 
-              const timeSlotArr = timeslots[index][0].slice(0, 5).split(":");
-              const slotTime = new Date();
+  //             const timeSlotArr = timeslots[index][0].slice(0, 5).split(":");
+  //             const slotTime = new Date();
 
-              currTime.setHours(Number(currTimeArr[0]), Number(currTimeArr[1]), 0);
-              slotTime.setHours(Number(timeSlotArr[0]), Number(timeSlotArr[1]), 0);
+  //             currTime.setHours(Number(currTimeArr[0]), Number(currTimeArr[1]), 0);
+  //             slotTime.setHours(Number(timeSlotArr[0]), Number(timeSlotArr[1]), 0);
 
-              if (currTime < slotTime) {
-                thisWeekToday[timeslots[index][0]] = timeslots[index][1];
-              } else {
-                nextWeekToday[timeslots[index][0]] = timeslots[index][1];
-              }
-            }
+  //             if (currTime < slotTime) {
+  //               thisWeekToday[timeslots[index][0]] = timeslots[index][1];
+  //             } else {
+  //               nextWeekToday[timeslots[index][0]] = timeslots[index][1];
+  //             }
+  //           }
 
-            if (Object.keys(thisWeekToday).length > 0) thisWeek[weekday] = thisWeekToday;
-            if (Object.keys(nextWeekToday).length > 0) nextWeek[weekday] = nextWeekToday;
-          } else {
-            thisWeek[weekday] = daySlots;
-          }
-        }
-      }
-    }
-    setTimetable([thisWeek, nextWeek]);
-  };
+  //           if (Object.keys(thisWeekToday).length > 0) thisWeek[weekday] = thisWeekToday;
+  //           if (Object.keys(nextWeekToday).length > 0) nextWeek[weekday] = nextWeekToday;
+  //         } else {
+  //           thisWeek[weekday] = daySlots;
+  //         }
+  //       }
+  //     }
+  //   }
+  //   setTimetable([thisWeek, nextWeek]);
+  // };
 
   useEffect(() => {
     let timeTicker = setInterval(() => {
-      setTimeNow(new Date().toString());
+      setTimeNow(new Date().toUTCString());
     }, 1000);
 
     if (toBookDoctor) {
@@ -118,11 +119,13 @@ export default function BookingsDoctor() {
   }, []);
 
   useEffect(() => {
-    if (chosenDoctor && chosenDoctor.available_times) available_times_Parser(chosenDoctor.available_times);
+    // if (chosenDoctor && chosenDoctor.available_times) available_times_Parser(chosenDoctor.available_times);
   }, [chosenDoctor]);
 
   useEffect(() => {
     console.log("TIMETABLE-USEEFFECT", timetable);
+    // console.log("today:", [weekdayOrder.find((ele) => ele.slice(0, 3) === value["$d"].toString().slice(0, 3).toLowerCase())]);
+    console.log("today:", value["$d"]);
   }, [timetable]);
 
   const handleSubmit = () => {
@@ -159,7 +162,7 @@ export default function BookingsDoctor() {
         <Box sx={{ display: "flex" }}>
           <Box sx={{ width: "900px", display: "flex", flexWrap: "wrap", alignItems: "center" }}>
             {/* <Typography>{ele[0]}</Typography> */}
-            {timetable.length > 0
+            {/* {timetable
               ? Object.entries(timetable[0][weekdayOrder.find((ele) => ele.slice(0, 3) === value["$d"].toString().slice(0, 3).toLowerCase())]).map(
                   (button, idx) => (
                     <button
@@ -169,7 +172,12 @@ export default function BookingsDoctor() {
                       }
                       style={{
                         width: "140px",
-                        backgroundColor: button[1] && button[0] !== bookedSlot[1] ? "#6dce6d" : button[0] === bookedSlot[1] ? "#1c58b1" : "#f4d5da",
+                        backgroundColor:
+                          button[1] && bookedSlot[1] && button[0] !== bookedSlot[1]
+                            ? "#6dce6d"
+                            : bookedSlot[1] && button[0] === bookedSlot[1]
+                            ? "#1c58b1"
+                            : "#f4d5da",
                         padding: "7px 15px",
                         border: "1px solid grey",
                         borderRadius: "3px",
@@ -181,7 +189,7 @@ export default function BookingsDoctor() {
                     </button>
                   )
                 )
-              : null}
+              : null} */}
           </Box>
         </Box>
         <HealMeButtonCustom onClick={handleSubmit} disabled={loading}>
